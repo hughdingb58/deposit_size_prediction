8dec360db637dac1c37762d2c2f56d163df7fcc9
Hugh Ding <hugh.ding@mail.utoronto.ca>
2024-04-20 19:24:12 -0400

    final code review

--- scripts/gather_api_data.py
+++ scripts/gather_api_data.py
@@ -10,12 +10,12 @@ from pyarrow.feather import read_feather, write_feather
 # FRED
 # ***************
 
-print("---FRED---")
+print("Gathering FRED data")
 
 # Read in FRED API key
 f = open("../fred_api_key.txt", "r")
 api_key = f.read().strip()
-fred_con = Fred(api_key = api_key)
+fred_con = Fred(api_key=api_key)
 
 # Loop through each series code and pull data
 fred_series = {
@@ -45,23 +45,22 @@ fred_series = {
     "AHETPI": "avg_hrly_earnings_production_nonsupervisory_employees",
     "MEHOINUSA672N": "real_median_household_income",
     "PCEC96": "pce",
-    "TOTALSA": "total_vehicle_sales"
+    "TOTALSA": "total_vehicle_sales",
 }
 
 econ_full = pd.DataFrame()
 
 for code in fred_series.keys():
-    print(code)
-    series = fred_con.get_series(code,
-                                 observation_start = "2000-01-01",
-                                 observation_end = "2023-12-31")
+    series = fred_con.get_series(
+        code, observation_start="2000-01-01", observation_end="2023-12-31"
+    )
     econ_full[code] = series
 
 # Rename columns to be more descriptive
-econ = econ_full.rename(columns = fred_series)
+econ = econ_full.rename(columns=fred_series)
 
 # Keep date as column
-econ = econ.reset_index().rename(columns = {"index": "date"})
+econ = econ.reset_index().rename(columns={"index": "date"})
 # Add year and quarter columns for merging
 econ["year"] = econ["date"].dt.year
 econ["quarter"] = econ["date"].dt.quarter
@@ -73,7 +72,7 @@ write_feather(econ, "../data/fred_econ.feather")
 # FDIC
 # ***************
 
-print("---FDIC---")
+print("Gathering FDIC data (approx. 30 min)")
 
 # FDIC data is gathered from its API - https://banks.data.fdic.gov/docs/
 fdic_url = "https://banks.data.fdic.gov/api/"
@@ -86,28 +85,26 @@ institutions_params = {
     "sort_by": "OFFICES",
     "sort_order": "DESC",
     "limit": 10000,
-    "format": "json"
+    "format": "json",
 }
 
-res = requests.get(institutions_url, params = institutions_params)
+res = requests.get(institutions_url, params=institutions_params)
 institutions_data = res.json()["data"]
 
 # Read data into list
 institutions_list = []
 for row in institutions_data:
-    curr_bank = pd.DataFrame(row["data"], index = [0])
+    curr_bank = pd.DataFrame(row["data"], index=[0])
     institutions_list.append(curr_bank)
 
 # Bind list into table
-institutions_full = pd.concat(institutions_list, axis = 0).reset_index(drop = True)
+institutions_full = pd.concat(institutions_list, axis=0).reset_index(drop=True)
 
 # Keep only relevant columns
 institutions = institutions_full[["NAMEHCR", "ZIP"]].drop_duplicates()
-institutions = institutions[institutions["NAMEHCR"] != ""].reset_index(drop = True)
+institutions = institutions[institutions["NAMEHCR"] != ""].reset_index(drop=True)
 institutions["ZIP"] = institutions["ZIP"].astype(str)
 institutions.head()
-
-print("Number of active institutions: " + str(institutions.shape[0]))
 
 # Create string of needed fields for API call
 fdic_series = {
@@ -124,13 +121,11 @@ fdic_series = {
     "EINTXQA": "total_interest_expense_quarterly",
     "NONIXQA": "non_interest_expense_quarterly",
     "DEP": "total_deposits",
-    "SC1LES": "debt_securities_maturity_1yr_or_less"
+    "SC1LES": "debt_securities_maturity_1yr_or_less",
 }
 
 fields_str = ",".join(fdic_series.keys())
 fields_sum_str = ",".join(list(fdic_series.keys())[4:])
-print(fields_str)
-print(fields_sum_str)
 
 financials_url = fdic_url + "financials"
 
@@ -140,36 +135,36 @@ financials_list = []
 for i in range(institutions.shape[0]):
     institution_row = institutions.iloc[i]
     financials_params = {
-        "filters": 'NAMEHCR:"' + str(institution_row["NAMEHCR"]) + '",ZIP:"' + str(institution_row["ZIP"]) + '"',
+        "filters": 'NAMEHCR:"'
+        + str(institution_row["NAMEHCR"])
+        + '",ZIP:"'
+        + str(institution_row["ZIP"])
+        + '"',
         "fields": fields_str,
         "sort_by": "REPDTE",
         "sort_order": "DESC",
         "limit": 10000,
         "agg_term_fields": "NAMEHCR,ZIP,REPDTE",
         "agg_sum_fields": fields_sum_str,
-        "format": "json"
+        "format": "json",
     }
 
     # Get list of institutions from API
-    res = requests.get(financials_url, params = financials_params)
+    res = requests.get(financials_url, params=financials_params)
     financials_data = res.json()["data"]
-    
+
     # Read data into list
     for row in financials_data:
-        curr_bank = pd.DataFrame(row["data"], index = [0])
+        curr_bank = pd.DataFrame(row["data"], index=[0])
         financials_list.append(curr_bank)
 
-    # To keep track of progress
-    if (i % 200) == 0:
-        print("row " + str(i) + ": " + str(institution_row["NAMEHCR"]))
-
 # Bind list into table
-financials_full = pd.concat(financials_list, axis = 0).reset_index(drop = True)
+financials_full = pd.concat(financials_list, axis=0).reset_index(drop=True)
 
 # Set more descriptive column names
-financials = financials_full.rename(columns = fdic_series)
+financials = financials_full.rename(columns=fdic_series)
 # Clean column data types
-financials["date"] = pd.to_datetime(financials["date"], format = "%Y%m%d")
+financials["date"] = pd.to_datetime(financials["date"], format="%Y%m%d")
 financials["zip"] = financials["zip"].astype(str)
 # Filter to holding companies only
 financials = financials[financials["classcode"] <= 65]
@@ -186,10 +181,21 @@ financials = financials.sort_values(["name", "zip", "date"])
 # The office counts don't seem to be properly filled in.
 # RoA and RoE are ratios, so they will be difficult to aggregate.
 # We no longer need classcode.
-financials = financials.drop(["ID", "offices_foreign", "offices_us",
-                              "roe_quarterly", "roa_quarterly",
-                              "zip", "classcode"], axis = 1)
+financials = financials.drop(
+    [
+        "ID",
+        "offices_foreign",
+        "offices_us",
+        "roe_quarterly",
+        "roa_quarterly",
+        "zip",
+        "classcode",
+    ],
+    axis=1,
+)
 # Aggregate by bank name
-financials = financials.groupby(["name", "date", "year", "quarter"]).agg("sum").reset_index()
+financials = (
+    financials.groupby(["name", "date", "year", "quarter"]).agg("sum").reset_index()
+)
 # Cache values
 write_feather(financials, "../data/fdic_financials.feather")
